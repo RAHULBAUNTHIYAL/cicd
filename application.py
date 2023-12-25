2eba5f1965714ecbd9777fb388f1a508b87d3ac8
RAHUL BAUNTHIYAL <raahulbaunthiyal48@gmail.com>
2023-12-25 13:06:16 +0530

    Update application.py

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
-app=Flask(__name__)
+application=Flask(__name__)
 ## Load the model
 regmodel=pickle.load(open('regmodel.pkl','rb'))
 scalar=pickle.load(open('scaling.pkl','rb'))
@@ -34,6 +34,6 @@ def predict():
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    application.run(host="0.0.0.0")
    
      
